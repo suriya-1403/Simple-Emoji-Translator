0a9f5bcdd7d4f544a850a3ea0d436edacb828386
suriyakrishnan <suriya@outlook.in>
2020-07-30 02:06:48 +0530

    Initial Commit

--- Simple Emoji Translator/ViewController.swift
+++ Simple Emoji Translator/ViewController.swift
@@ -14,6 +14,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func showmessage(sender: UIButton){
+        var emdic: [String: String] = [ "😂": "Ha..Ha..",
+                                        "🐲": "Dragon",
+                                        "👻": "Snapghost",
+                                        "🃏": "Jocker",
+                                        "🤯": "Brain-Busted"]
+        
+        let selbut = sender
+        
+        let containerView = UIView(frame: CGRect(x: 0,y: 0,width: 300, height: 300))
+        containerView.backgroundColor = UIColor.gray
+        
+        let emlab = UILabel(frame: CGRect(x: 90,y: 30,width: 150,height: 150))
+        emlab.font = UIFont.systemFont(ofSize: 100)
+    }
 
 
 }

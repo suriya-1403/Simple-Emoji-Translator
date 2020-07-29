fb38371f5c761389c88c4ad5595aa89f9bdcfa4b
SuriyaKrishnan <63546116+suriya-1403@users.noreply.github.com>
2020-07-30 01:21:39 +0530

    Initial Commit

---
+++ Simple Emoji Translator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple Emoji Translator
+//
+//  Created by Suriya Krishnan on 30/07/20.
+//  Copyright © 2020 Suriya Krishnan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Simple Emoji TranslatorTests/Simple_Emoji_TranslatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  Simple_Emoji_TranslatorTests.swift
+//  Simple Emoji TranslatorTests
+//
+//  Created by Suriya Krishnan on 30/07/20.
+//  Copyright © 2020 Suriya Krishnan. All rights reserved.
+//
+
+import XCTest
+@testable import Simple_Emoji_Translator
+
+class Simple_Emoji_TranslatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
